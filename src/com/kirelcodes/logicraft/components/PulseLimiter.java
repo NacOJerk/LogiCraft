3acb87c3707ee930c5049eaebfe08e5675952ea5
Gal Horowitz <galush.horowitz@gmail.com>
2016-08-26 18:13:43 +0300

    PulseLimiter

---
+++ src/com/kirelcodes/logicraft/components/PulseLimiter.java
@@ -0,0 +1,77 @@
+package com.kirelcodes.logicraft.components;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.kirelcodes.logicraft.LogiCraft;
+import com.kirelcodes.logicraft.utils.NBTRW;
+
+public class PulseLimiter extends Component{
+
+	public PulseLimiter(Location loc) {
+		super(loc);
+		getComponent().setHelmet(new ItemStack(Material.SOUL_SAND));
+		getComponent().setCustomName("Pulse Limiter".toUpperCase());
+		getComponent().setCustomNameVisible(true);
+		setStoredByte(false);
+	}
+	
+	public PulseLimiter(ArmorStand armor) {
+		super(armor);
+	}
+	
+	private boolean onOff(){
+		ItemStack dataSaver = getComponent().getChestplate();
+		try{
+			if(!NBTRW.containsNBTTag(dataSaver, "storedByte")){
+				return false;
+			}
+			return NBTRW.getNBTBoolean(dataSaver, "storedByte");
+		}catch(Exception e){
+			return false;
+		}
+	}
+	
+	private void setStoredByte(boolean onOf){
+		ItemStack dataSaver = getComponent().getChestplate();
+		try{
+			dataSaver = NBTRW.writeNBT(dataSaver, "storedByte", onOf);
+			if(onOf)
+				getComponent().setHelmet( new ItemStack(Material.JACK_O_LANTERN));
+			else
+				getComponent().setHelmet( new ItemStack(Material.PUMPKIN));
+			getComponent().setChestplate(dataSaver);
+		}catch(Exception e){
+			return;
+		}
+
+	}
+
+	@Override
+	public void redstoneUpdate() {
+		if(checkForConnection(getLocation())>1){
+			if(onOff()){
+				turnLever(getLocation(), false);
+			}else{
+				turnLever(getLocation(), true);
+				new BukkitRunnable() {
+					
+					@Override
+					public void run() {
+						setStoredByte(true);
+					}
+				}.runTaskLater(LogiCraft.getInstance(), 1L);
+				
+			}
+		}else{
+			if(onOff()){
+				turnLever(getLocation(), false);
+				setStoredByte(false);
+			}
+		}
+	}
+
+}
